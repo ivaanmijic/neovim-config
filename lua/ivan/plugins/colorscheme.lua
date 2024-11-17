b26c8f3ec5605654c85e395cb25e6414beb22f57
ivaanmijic <imijic02@gmail.com>
2024-11-17 10:06:56 +0100

    add vimtex, change colorscheme.lua

--- lua/ivan/plugins/colorscheme.lua
+++ lua/ivan/plugins/colorscheme.lua
@@ -3,9 +3,9 @@ return {
   priority = 1000,
   config = function()
     vim.g.material_style = "deep ocean"
-    require('material').setup({
+    require("material").setup({
 
-    contrast = {
+      contrast = {
         terminal = false, -- Enable contrast for the built-in terminal
         sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
         floating_windows = false, -- Enable contrast for floating windows
@@ -13,19 +13,16 @@ return {
         lsp_virtual_text = false, -- Enable contrasted background for lsp virtual text
         non_current_windows = false, -- Enable contrasted background for non-current windows
         filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
-    },
-    --
-    -- styles = { -- Give comments style such as bold, italic, underline etc.
-    --     comments = { italic = true },
-    --     strings = { italic = true },
-    --     keywords = { bold = true },
-    --     functions = { --[[ bold = true ]] },
-    --     variables = { --[[ bold = true  ]]},
-    --     operators = {},
-    --     types = {},
-    -- },
+      },
+      --
+      styles = { -- Give comments style such as bold, italic, underline etc.
+        comments = { italic = true },
+        keywords = { bold = true },
+        functions = { bold = true, italic = true },
+        variables = { bold = true },
+      },
 
-    plugins = { -- Uncomment the plugins that you use to highlight them
+      plugins = { -- Uncomment the plugins that you use to highlight them
         -- Available plugins:
         -- "coc",
         -- "colorful-winsep",
@@ -56,31 +53,31 @@ return {
         -- "trouble",
         -- "which-key",
         -- "nvim-notify",
-    },
+      },
 
-    disable = {
+      disable = {
         colored_cursor = false, -- Disable the colored cursor
         borders = false, -- Disable borders between vertically split windows
         background = false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
         term_colors = false, -- Prevent the theme from setting terminal colors
-        eob_lines = false -- Hide the end-of-buffer lines
-    },
+        eob_lines = false, -- Hide the end-of-buffer lines
+      },
 
-    high_visibility = {
+      high_visibility = {
         lighter = false, -- Enable higher contrast text for lighter style
-        darker = false -- Enable higher contrast text for darker style
-    },
+        darker = false, -- Enable higher contrast text for darker style
+      },
 
-    lualine_style = "stealth", -- Lualine style ( can be 'stealth' or 'default' )
+      lualine_style = "stealth", -- Lualine style ( can be 'stealth' or 'default' )
 
-    async_loading = true, -- Load parts of the theme asynchronously for faster startup (turned on by default)
+      async_loading = true, -- Load parts of the theme asynchronously for faster startup (turned on by default)
 
-    custom_colors = nil, -- If you want to override the default colors, set this to a function
+      custom_colors = nil, -- If you want to override the default colors, set this to a function
 
-    custom_highlights = {}, -- Overwrite highlights with your own
+      custom_highlights = {}, -- Overwrite highlights with your own
     })
-   
+
     vim.cmd("colorscheme material")
-    vim.cmd [[hi Normal guibg=NONE ctermbg=NONE]]
-  end
+    vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
+  end,
 }
